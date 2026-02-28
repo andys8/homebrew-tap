810c63b08e4868841df8abbe13f2fb4c69496ad2
Andy <andys8@users.noreply.github.com>
2026-02-28 15:28:05 +0100

    Update git-brunch to v1.8.0

--- Formula/git-brunch.rb
+++ Formula/git-brunch.rb
@@ -1,9 +1,9 @@
 class GitBrunch < Formula
   desc "Git checkout command-line tool"
   homepage "https://github.com/andys8/git-brunch"
-  url "https://github.com/andys8/git-brunch/releases/download/v1.7.2/git-brunch-macOS"
-  version "1.7.2"
-  sha256 "4776a48672c44a855498a74b728360e76af7f792344d4659b037c6baae24b819"
+  url "https://github.com/andys8/git-brunch/releases/download/v1.8.0/git-brunch-macOS"
+  version "1.8.0"
+  sha256 "b5aa22421fae2f379e212b677892cf5d40d0bb44d1950ec132eb0ae6e2c75bf2"
 
   def install
     bin.install "git-brunch-macOS" => "git-brunch"
